6f3817d19447229fc57667fbb7c44c4cbb2d105c
xiaoluoboding <xiaoluoboding@gmail.com>
2020-07-14 23:11:53 +0800

    docs: update js-algorithms

---
+++ algorithms/leetcode/0150.evaluate-reverse-polish-notation.js
@@ -0,0 +1,67 @@
+/**
+ * @problem 150. 逆波兰表达式求值
+ * @param {string[]} tokens
+ * @return {number}
+ * @description
+ * 输入：["2", "1", "+", "3", "*"]
+ * 输出：9
+ * 解释: 该算式转化为常见的中缀算术表达式为：((2 + 1) * 3) = 9
+ * @tip 适合用栈操作运算：遇到数字则入栈；遇到算符则取出栈顶两个数字进行计算，并将结果压入栈中。
+ */
+var evalRPN = function (tokens) {
+  let stack = []
+  let numRight
+  let numLeft
+
+  for (let token of tokens) {
+    switch (token) {
+      case '+':
+        stack.push(stack.pop() + stack.pop())
+        break
+      case '-':
+        numRight = stack.pop()
+        numLeft = stack.pop()
+        stack.push(numLeft - numRight)
+        break
+      case '*':
+        stack.push(stack.pop() * stack.pop())
+        break
+      case '/':
+        numRight = stack.pop()
+        numLeft = stack.pop()
+        stack.push(parseInt((numLeft / numRight), 10))
+        break
+      default:
+        stack.push(parseInt(token, 10))
+    }
+  }
+
+  return stack.pop()
+}
+
+/**
+ * 解法2，在leetcode上执行效率更高
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+  const map = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => parseInt(a / b, 10)
+  }
+
+  let fun = () => {
+    let char = tokens.pop()
+    if (map[char]) {
+      let numRight = fun()
+      let numLeft = fun()
+      return map[char](numLeft, numRight)
+    } else {
+      return parseInt(char, 10)
+    }
+  }
+
+  return fun()
+}
--- algorithms/leetcode/jz59.max-sliding-window.js
+++ algorithms/leetcode/jz59.max-sliding-window.js
@@ -1,24 +1,44 @@
 /**
- * 剑指 Offer 59 - I. 滑动窗口的最大值
+ * @problem 剑指 Offer 59 - I. 滑动窗口的最大值
  * @param {number[]} nums
  * @param {number} k
  * @return {number[]}
+ * @description
+ * 输入: nums = [1, 3, -1, -3, 5, 3, 6, 7], 和 k = 3
+ * 输出: [3, 3, 5, 5, 6, 7]
+ * 解释:
+ * 
+ *  滑动窗口的位置                最大值
+ *  -- -- -- -- -- -- -- -- -- -- -- --
+ *  [1, 3, -1,] -3, 5, 3, 6, 7      3
+ *  1, [3, -1, -3,] 5, 3, 6, 7      3
+ *  1, 3, [-1, -3, 5,] 3, 6, 7      5
+ *  1, 3, -1, [-3, 5, 3,] 6, 7      5
+ *  1, 3, -1, -3, [5, 3, 6,] 7      6
+ *  1, 3, -1, -3, 5, [3, 6, 7]      7
+ *  -- -- -- -- -- -- -- -- -- -- -- --
  */
 var maxSlidingWindow = function(nums, k) {
+  // 记录最大值的数组
   let max = []
+  // 声明一个双端队列
   let deque = []
 
   for (let i = 0; i < nums.length; i++) {
-    while (deque.length && nums[deque[deque.length -1]] < nums[i]) {
+    // 单调栈模版套起来，队头永远保持存储最大元素的索引，类似问题：496. 下一个更大元素 I
+    while (deque.length && nums[deque[deque.length - 1]] < nums[i]) {
+      // 队尾出队保持递减
       deque.pop()
     }
 
     deque.push(i)
 
+    // 将窗口外的队头元素索引出队
     while (deque.length && deque[0] <= i - k) {
       deque.shift()
     }
 
+    // 形成窗口开始，记录队列头部最大值, max 的长度等于 nums.length - k + 1
     if (i + 1 >= k) max.push(nums[deque[0]])
   }
 
