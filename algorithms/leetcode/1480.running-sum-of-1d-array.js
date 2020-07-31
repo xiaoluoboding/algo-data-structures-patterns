ab499dc332365f5006f8066bb7b4f66c6a08f054
xiaoluoboding <xiaoluoboding@gmail.com>
2020-07-31 10:06:14 +0800

    docs: update js-algorithms

---
+++ algorithms/leetcode/1480.running-sum-of-1d-array.js
@@ -0,0 +1,54 @@
+/**
+ * @lc app=leetcode.cn id=1480 lang=javascript
+ * @problem https://leetcode-cn.com/problems/running-sum-of-1d-array/description/
+ * [1480] 一维数组的动态和
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * @tip 哈希表解法
+ * hash: {
+ *  '0': 1 + 0,
+ *  '1': 2 + hash[0],
+ *  '2': 3 + hash[1],
+ *  '3': 4 + hash[2]
+ * }
+ */
+var runningSum = function(nums) {
+  let hash = {}
+
+  for (let i = 0; i < nums.length; i++) {
+    // 注意哈希表存入的是下标
+    if (!hash[i]) {
+      let prev = i === 0 ? 0 : hash[i - 1]
+      hash[i] = nums[i] + prev
+    }
+  }
+
+  return Object.values(hash)
+}
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * @tip 哈希表解法
+ * hash: {
+ *  '0': 1 + 0,
+ *  '1': 2 + hash[0],
+ *  '2': 3 + hash[1],
+ *  '3': 4 + hash[2]
+ * }
+ */
+var runningSum = function(nums) {
+  let hash = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    // 注意哈希表存入的是下标
+    if (!hash.has(i)) {
+      let prev = i === 0 ? 0 : hash.get(i - 1)
+      hash.set(i, nums[i] + prev)
+    }
+  }
+
+  return Array.from(hash.values())
+}
