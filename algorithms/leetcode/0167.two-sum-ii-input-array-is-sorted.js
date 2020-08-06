b5ddce511711c9a5919ff9e50746d260e13346f3
xiaoluoboding <xiaoluoboding@gmail.com>
2020-08-06 17:57:14 +0800

    docs: update js-algorithms

--- algorithms/leetcode/0167.two-sum-ii-input-array-is-sorted.js
+++ algorithms/leetcode/0167.two-sum-ii-input-array-is-sorted.js
@@ -1,4 +1,4 @@
-/*
+/**
  * @lc app=leetcode.cn id=167 lang=javascript
  * @problem https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/description/
  * [167] 两数之和 II - 输入有序数组
---
+++ algorithms/leetcode/binary-tree/0098.validate-binary-search-tree.js
@@ -0,0 +1,30 @@
+/**
+ * @lc app=leetcode.cn id=98 lang=javascript
+ * @problem https://leetcode-cn.com/problems/validate-binary-search-tree/description/
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+  const dfs = (root, minValue, maxValue) => {
+    if (!root) return true
+    
+    if (root.val <= minValue || root.val >= maxValue) return false
+
+    return dfs(root.left, minValue, root.val) && dfs(root.right, root.val, maxValue)
+  }
+  return dfs(root, -Infinity, Infinity)
+};
+// @lc code=end
+
